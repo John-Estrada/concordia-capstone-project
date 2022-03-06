89c60f54811dbe3b78016f87cc7dc2bd7502b29f
John Estrada <john@Johns-MacBook-Pro.local>
2022-03-06 07:00:54 -0500

    database updates

--- server/server/settings.py
+++ server/server/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('db_name'),
+        'USER': os.getenv('db_user'),
+        'PASSWORD': os.getenv('db_password'),
+        'HOST':'',
+        'PORT':'',
     }
 }
 
